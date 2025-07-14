c4322469f4f5fb2c53cacedb8f6478d5fb31b78b
Mike Herman <mhermedu@proton.me>
2025-07-13 20:38:18 -0400

    Flutter frontend added
    Added flutter front end to project

---
+++ one_step_app_frontend/lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+import 'package:one_step_app_flutter/HomeDashboardScreen.dart';
+import 'register_login_selection.dart';
+import 'HomeDashboardScreen.dart';
+
+
+void main() {
+  runApp(
+     MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: HomeDashboardScreen(),
+    ),
+  );
+}
